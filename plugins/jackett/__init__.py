c47c017441ee83b1d65043cd6326f95e15a87ea4
君禹渊 <taoxu2870@outlook.com>
2024-06-01 11:28:09 +0800

    还原domain与Search

--- plugins/jackett/__init__.py
+++ plugins/jackett/__init__.py
@@ -26,7 +26,7 @@ class Jackett(_PluginBase):
     # 主题色
     plugin_color = "#000000"
     # 插件版本
-    plugin_version = "0.0.20"
+    plugin_version = "0.0.23"
     # 插件作者
     plugin_author = "Junyuyuan,Ray"
     # 作者主页
