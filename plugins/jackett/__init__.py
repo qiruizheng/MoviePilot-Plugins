a13b470ade34e3a91430498180262784b8548b11
Mogeko <taoxu2870@outlook.com>
2024-06-03 16:37:11 +0800

    测试indexerConf2

--- plugins/jackett/__init__.py
+++ plugins/jackett/__init__.py
@@ -26,7 +26,7 @@ class Jackett(_PluginBase):
     # 主题色
     plugin_color = "#000000"
     # 插件版本
-    plugin_version = "0.0.29"
+    plugin_version = "0.0.36"
     # 插件作者
     plugin_author = "Junyuyuan,Ray"
     # 作者主页
