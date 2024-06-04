7dfa5cb490c178f60c61eb49be08993d74fed4b5
君禹渊 <taoxu2870@outlook.com>
2024-06-04 22:02:59 +0800

    list添加一层

--- plugins/jackett/__init__.py
+++ plugins/jackett/__init__.py
@@ -26,7 +26,7 @@ class Jackett(_PluginBase):
     # 主题色
     plugin_color = "#000000"
     # 插件版本
-    plugin_version = "0.2.0"
+    plugin_version = "0.2.1"
     # 插件作者
     plugin_author = "Junyuyuan,Ray"
     # 作者主页
@@ -179,7 +179,7 @@ class Jackett(_PluginBase):
                         ]
                     },
                     "torrents": {
-                        "list": {"selector": "item"},
+                        "list": {"selector": "channel > item"},
                         "fields": {
                             "id": {
                                 "selector": "jackettindexer",
