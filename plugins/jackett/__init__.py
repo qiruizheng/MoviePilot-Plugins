71053e11687a3d17a43bf7328b1e4e7953d61525
qiruizheng <4271439+qiruizheng@users.noreply.github.com>
2024-06-08 23:17:27 +0800

    Update __init__.py

--- plugins/jackett/__init__.py
+++ plugins/jackett/__init__.py
@@ -169,7 +169,7 @@ class Jackett(_PluginBase):
                     "public": True,
                     "proxy": True,
                     "result_num": 100,
-                    "timeout": 30,
+                    "timeout": 60,
                     "search": {
                         "paths": [
                             {
@@ -216,7 +216,30 @@ class Jackett(_PluginBase):
         except Exception as e:
             logger.error(f"获取索引器失败：{str(e)}")
             return []
+            
+    def search(self, indexer,
+               keyword,
+               page):
+        """
+        根据关键字多线程检索
+        """
+        if not indexer or not keyword:
+            return None
+        self.info(f"【{self.plugin_name}】开始检索Indexer：{indexer.name} ...")
+        # 特殊符号处理
+        api_url = f"{indexer.domain}?apikey={self._api_key}&t=search&q={keyword}"
 
+        result_array = self.__parse_torznabxml(api_url)
+
+        if len(result_array) == 0:
+            self.warn(f"【{self.plugin_name}】{indexer.name} 未检索到数据")
+            # self.progress.update(ptype='search', text=f"{indexer.name} 未检索到数据")
+            return []
+        else:
+            self.warn(f"【{self.plugin_name}】{indexer.name} 返回数据：{len(result_array)}")
+            return result_array
+
+    
     def get_fake_site(self):
         pass
 
@@ -377,7 +400,110 @@ class Jackett(_PluginBase):
                 "run_once": False,
             },
         )
+        
+  @staticmethod
+    def __parse_torznabxml(url):
+        """
+        从torznab xml中解析种子信息
+        :param url: URL地址
+        :return: 解析出来的种子信息列表
+        """
+        if not url:
+            return []
+        try:
+            ret = RequestUtils(timeout=10).get_res(url)
+        except Exception as e2:
+            ExceptionUtils.exception_traceback(e2)
+            return []
+        if not ret:
+            return []
+        xmls = ret.text
+        if not xmls:
+            return []
 
+        torrents = []
+        try:
+            # 解析XML
+            dom_tree = xml.dom.minidom.parseString(xmls)
+            root_node = dom_tree.documentElement
+            items = root_node.getElementsByTagName("item")
+            for item in items:
+                try:
+                    # indexer id
+                    indexer_id = DomUtils.tag_value(item, "jackettindexer", "id",
+                                                    default=DomUtils.tag_value(item, "prowlarrindexer", "id", ""))
+                    # indexer
+                    indexer = DomUtils.tag_value(item, "jackettindexer",
+                                                 default=DomUtils.tag_value(item, "prowlarrindexer", default=""))
+
+                    # 标题
+                    title = DomUtils.tag_value(item, "title", default="")
+                    if not title:
+                        continue
+                    # 种子链接
+                    enclosure = DomUtils.tag_value(item, "enclosure", "url", default="")
+                    if not enclosure:
+                        continue
+                    # 描述
+                    description = DomUtils.tag_value(item, "description", default="")
+                    # 种子大小
+                    size = DomUtils.tag_value(item, "size", default=0)
+                    # 种子页面
+                    page_url = DomUtils.tag_value(item, "comments", default="")
+
+                    # 做种数
+                    seeders = 0
+                    # 下载数
+                    peers = 0
+                    # 是否免费
+                    freeleech = False
+                    # 下载因子
+                    downloadvolumefactor = 1.0
+                    # 上传因子
+                    uploadvolumefactor = 1.0
+                    # imdbid
+                    imdbid = ""
+
+                    torznab_attrs = item.getElementsByTagName("torznab:attr")
+                    for torznab_attr in torznab_attrs:
+                        name = torznab_attr.getAttribute('name')
+                        value = torznab_attr.getAttribute('value')
+                        if name == "seeders":
+                            seeders = value
+                        if name == "peers":
+                            peers = value
+                        if name == "downloadvolumefactor":
+                            downloadvolumefactor = value
+                            if float(downloadvolumefactor) == 0:
+                                freeleech = True
+                        if name == "uploadvolumefactor":
+                            uploadvolumefactor = value
+                        if name == "imdbid":
+                            imdbid = value
+
+                    tmp_dict = {'indexer_id': indexer_id,
+                                'indexer': indexer,
+                                'title': title,
+                                'enclosure': enclosure,
+                                'description': description,
+                                'size': size,
+                                'seeders': seeders,
+                                'peers': peers,
+                                'freeleech': freeleech,
+                                'downloadvolumefactor': downloadvolumefactor,
+                                'uploadvolumefactor': uploadvolumefactor,
+                                'page_url': page_url,
+                                'imdbid': imdbid}
+                    torrents.append(tmp_dict)
+                except Exception as e:
+                    ExceptionUtils.exception_traceback(e)
+                    continue
+        except Exception as e2:
+            ExceptionUtils.exception_traceback(e2)
+            pass
+
+        return torrents
+        
     def get_page(self) -> List[dict] | None:
         pass
 
